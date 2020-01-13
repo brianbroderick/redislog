baf87de8627f4223d99696332cbdaefecec3ed54
Brian Broderick <brian.broderick@podium.com>
2020-01-13 15:41:54 -0700

    Make this compile for PG 12

--- redislog.c
+++ redislog.c
@@ -65,8 +65,8 @@ int   Redislog_timeout = 1000;
 char  *Redislog_key = NULL;
 int   Redislog_min_error_statement = ERROR;
 int   Redislog_min_messages = WARNING;
-bool  Redislog_ship_to_redis_only = TRUE;
-bool  Redislog_shuffle_hosts = TRUE;
+bool  Redislog_ship_to_redis_only = true;
+bool  Redislog_shuffle_hosts = true;
 char  *Redislog_fields = NULL;
 
 static MemoryContext	redislog_cfg_memory_context;
@@ -1357,7 +1357,7 @@ _PG_init(void)
 	  "in case no Redis service is available. "
 	  "By default it is set to false.",
 	  &Redislog_ship_to_redis_only,
-	  FALSE,
+	  false,
 	  PGC_SUSET,
 	  GUC_NOT_IN_SAMPLE,
 	  NULL,
@@ -1369,7 +1369,7 @@ _PG_init(void)
 	  "Shuffle the list of available Redis server in order to"
 	  "balance events servers",
 	  &Redislog_shuffle_hosts,
-	  TRUE,
+	  true,
 	  PGC_SUSET,
 	  GUC_NOT_IN_SAMPLE,
 	  NULL,
